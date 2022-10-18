f060678b5aa96cafb5112a1353d34485ddf2fcbe
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-10-18 19:42:45 +0200

    add correct space

--- recrutas.js
+++ recrutas.js
@@ -2,3 +2,10 @@ const firstName = prompt("First Name:");
 const lastName = prompt("Last Name:");
 const studyArea = prompt("Your area of ​study:");
 const birth = prompt("Your date of birth:");
+
+alert(
+  "Soldier successfully registered!\n" +
+  "\n First name: " + firstName + " " + lastName +
+  "\n Study Area: " + studyArea +
+  "\n Age: " + (2022 - birth)
+)
