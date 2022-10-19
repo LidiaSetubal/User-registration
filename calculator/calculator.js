e3bf9cea19160c8de16ccaf657194eb7cf3aa9af
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-10-19 19:16:53 +0200

    operations

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,1 +1,18 @@
+const num1 = prompt("Enter first number:");
+const num2 = prompt("Enter second number:");
 
+const x = parseFloat(num1);
+const y =parseFloat(num2);
+
+const sum = x + y
+const subtraction = x - y
+const multiplication = x * y
+const division = x / y
+
+alert(
+  "Results:\n" +
+  "\nSum: " + sum +
+  "\nSubtraction: " + subtraction +
+  "\nMultiplication: " + multiplication +
+  "\nDivision: " + division
+)
